a57056531710106677f439c8bf903a4395dc949e
dimasrizky29 <63985999+dimasrizky29@users.noreply.github.com>
2021-09-16 23:52:36 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
-$server = "localhost";
-$username = "root";
-$password = "";
+$server = "13.213.144.133";
+$username = "yatech";
+$password = "Yatech@5758";
 $db = "dogedatabase";
 
 $connect = new mysqli ($server, $username, $password, $db);
