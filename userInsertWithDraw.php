7c2d2536140b5f062a069d56476f704ccbbde8e2
LAPTOP-OO7BFUUT\Dimas <dimasrizkyaf@gmail.com>
2021-09-21 11:11:25 +0700

    heroku

---
+++ userInsertWithDraw.php
@@ -0,0 +1,19 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization, X-Requested-With");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+include ('connection.php');
+
+$Address = $_POST["addAddress"];
+$Date = isset($_POST["addDate"]) ? $_POST["addDate"]: "";
+$GamePlayed = isset($_POST["addGamePlayed"]) ? $_POST["addGamePlayed"]: "";
+$Status = "Claim First";
+$Balance = isset($_POST["addBalance"]) ? $_POST["addBalance"]: "";
+//print_r($_POST);
+
+$sql = "insert into withdraw (Address, Date, GamePlayed, Status, Balance) values ('".$Address."','".$Date."','".$GamePlayed."','".$Status."','".$Balance."')";
+$result = mysqli_query($connect, $sql);
+?>
